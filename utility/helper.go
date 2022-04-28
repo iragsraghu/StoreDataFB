5503c1578f4e9b21d9c5af2dd4d68f770bf736c0
iragsraghu <raghavendra@frontier.xyz>
2022-04-28 15:38:42 +0530

    commented

--- utility/helper.go
+++ utility/helper.go
@@ -2,7 +2,6 @@ package utility
 
 import (
 	"StoreDataFB/deviceInfo"
-	"runtime"
 )
 
 func TrimString(s string) string {
@@ -17,26 +16,26 @@ func TrimString(s string) string {
 
 func DeviceTypes() string {
 	var device_id string
-	if runtime.GOOS == "darwin" {
-		device_number := deviceInfo.DarwinDevice("Apple") // Generate serial number
-		device_id = TrimString(device_number)             // Trim the string
-	}
-	if runtime.GOOS == "linux" {
-		device_number := deviceInfo.LinuxDevice("Linux") // Generate serial number
-		device_id = TrimString(device_number)            // Trim the string
-	}
-	if runtime.GOOS == "windows" {
-		device_number := deviceInfo.WindowsDevice("Windows") // Generate serial number
-		device_id = TrimString(device_number)                // Trim the string
-	}
-	if runtime.GOOS == "android" {
-		device_number := deviceInfo.AndroidDevice("Android") // Generate serial number
-		device_id = TrimString(device_number)                // Trim the string
-	}
-	if runtime.GOOS == "ios" {
-		device_number := deviceInfo.IOSDevice("IOS") // Generate serial number
-		device_id = TrimString(device_number)        // Trim the string
-	}
+	// if runtime.GOOS == "darwin" {
+	device_number := deviceInfo.DarwinDevice("Apple") // Generate serial number
+	device_id = TrimString(device_number)             // Trim the string
+	// }
+	// if runtime.GOOS == "linux" {
+	// 	device_number := deviceInfo.LinuxDevice("Linux") // Generate serial number
+	// 	device_id = TrimString(device_number)            // Trim the string
+	// }
+	// if runtime.GOOS == "windows" {
+	// 	device_number := deviceInfo.WindowsDevice("Windows") // Generate serial number
+	// 	device_id = TrimString(device_number)                // Trim the string
+	// }
+	// if runtime.GOOS == "android" {
+	// 	device_number := deviceInfo.AndroidDevice("Android") // Generate serial number
+	// 	device_id = TrimString(device_number)                // Trim the string
+	// }
+	// if runtime.GOOS == "ios" {
+	// 	device_number := deviceInfo.IOSDevice("IOS") // Generate serial number
+	// 	device_id = TrimString(device_number)        // Trim the string
+	// }
 	return device_id
 }
 
