bd36095c34ccd72b7b0acab500fa3da28cbda43a
iragsraghu <raghavendra@frontier.xyz>
2022-04-28 13:38:20 +0530

    port added

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"StoreDataFB/controller/demo_controller"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	const port string = ":8000"
+	// const port string = ":8000"
 
 	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprintln(w, "App Running...")
@@ -20,6 +21,7 @@ func main() {
 	router.HandleFunc("/register", StoreDeviceData).Methods("POST")
 	router.HandleFunc("/listData", ListStoreData).Methods("GET")
 	router.HandleFunc("/referrals", demo_controller.ReferralsPage).Methods("GET")
-	log.Println("Server listening on port", port)
-	log.Fatalln(http.ListenAndServe(port, router))
+	// log.Println("Server listening on port", port)
+	// log.Fatalln(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
