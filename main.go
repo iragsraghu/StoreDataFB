2eb493dad44771477f2c4dfb8394a8d49a546e7e
iragsraghu <raghavendra@frontier.xyz>
2022-04-28 16:58:18 +0530

    dynamic data added

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"StoreDataFB/controller/demo_controller"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-	// const port string = ":8000"
+	const port string = ":8000"
 
 	// router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprintln(w, "App Running...")
@@ -21,7 +20,7 @@ func main() {
 	router.HandleFunc("/register", StoreDeviceData).Methods("POST")
 	router.HandleFunc("/listData", ListStoreData).Methods("GET")
 	router.HandleFunc("/referrals", demo_controller.ReferralsPage).Methods("GET")
-	// log.Println("Server listening on port", port)
-	// log.Fatalln(http.ListenAndServe(port, router))
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	log.Println("Server listening on port", port)
+	log.Fatalln(http.ListenAndServe(port, router))
+	// log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
